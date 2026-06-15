f0e6329367dff1ef3b602d8dd0ffee4d6937d4f6
quellerz <152263705+quellerz@users.noreply.github.com>
2026-06-15 02:27:18 +0200

    weapon auto pick-up when player's weapon is out of ammo disabled

--- src/game/server/hl2/hl2_player.cpp
+++ src/game/server/hl2/hl2_player.cpp
@@ -2713,11 +2713,12 @@ bool CHL2_Player::BumpWeapon( CBaseCombatWeapon *pWeapon )
 		{
 			CBaseCombatWeapon *pActiveWeapon = Weapon_GetSlot( WEAPON_PRIMARY_SLOT );
 
-			if ( pActiveWeapon != NULL && pActiveWeapon->HasAnyAmmo() == false && Weapon_CanSwitchTo( pWeapon ) )
-			{
-				Weapon_Equip( pWeapon );
-				return true;
-			}
+            /* This code is commended out because it causes player to pickup weapon from ANY slot when one of his weapon is out of ammo*/
+			/*if ( pActiveWeapon != NULL && pActiveWeapon->HasAnyAmmo() == false && Weapon_CanSwitchTo( pWeapon ) )*/
+			/*{*/
+				/*Weapon_Equip( pWeapon );*/
+				/*return true;*/
+			/*}*/
 
 			//Attempt to take ammo if this is the gun we're holding already
 			if ( Weapon_OwnsThisType( pWeapon->GetClassname(), pWeapon->GetSubType() ) )
